f6ae641774db7aa4ad3f4788f44211ce5f80e194
eflipe <felipecabaleiro@gmail.com>
2022-07-13 01:00:37 -0300

    [ADD] index.html

--- eadda_app/urls.py
+++ eadda_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import add_stories
+from .views import add_stories, index
 
 app_name = 'eadda_app'
 
 urlpatterns = [
+    path('',index, name="index"),
     path('add',add_stories, name="add_story")
     ]
--- eadda_app/views.py
+++ eadda_app/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from .models import Stories
 from .forms import StoryForm
 
+def index(request):
+    context_dict = {
+        'boldmessage': 'Prueba 1234'
+    }
+    template = 'eadda_app/index.html'
+    return render(request, template, context_dict)
+
+
 def add_stories(request):
     form = StoryForm
     return render(request, 'eadda_app/stories.html',{ "form" :form})
