d424ff6483f39bffcf1bf7e9ab0178679c1ac50c
eflipe <felipecabaleiro@gmail.com>
2022-08-19 21:47:18 -0300

    [MOD] linebreaks

--- mundial_project/settings.py
+++ mundial_project/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
